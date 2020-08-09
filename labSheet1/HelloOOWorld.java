3503704ee04e37f4789f5241758bbd29cc607656
Voxilous <57728240+JeremiSz@users.noreply.github.com>
2020-08-09 13:38:07 +0100

    first

---
+++ labSheet1/HelloOOWorld.java
@@ -0,0 +1,10 @@
+package labSheet1;
+
+import javax.swing.*;
+
+public class HelloOOWorld {
+    public static void main(String args[]){
+        System.out.print("Hello World");
+        JOptionPane.showMessageDialog(null,"Hello World!!","Exitment ahead!",JOptionPane.INFORMATION_MESSAGE);
+    }
+}
